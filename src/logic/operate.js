80bfecfa984fb64a0e7df66c873e497fe41f27ca
J. Christian Romero <jcromerohdz@gmail.com>
2020-04-13 14:09:33 -0700

    Implementig operate functionality

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,20 @@
 import Big from 'big-js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  const [firstNum, secondNum] = [Big(numberOne), Big(numberTwo)];
+  switch (operation) {
+    case '+':
+      return firstNum.plus(secondNum).toString();
+    case '-':
+      return firstNum.plus(secondNum).toString();
+    case '/':
+      if (secondNum === '0') return '0';
+      return firstNum.div(secondNum).toString();
+    case 'X':
+      return firstNum.times(secondNum).toString();
+    default:
+      return '0';
+  }
+};
+
+export default operate;
